2c638524965f232bc926e07da9b1092c0967b56e
Dominion Gbadamosi <worshipgbadamosi@live.com>
2025-08-11 19:03:33 +0100

    add: Clipboard manager now supports text handling + created a ClipboardHashingException for hashing at runtime

--- client/src/main/java/com/cuhlippa/client/clipboard/ClipboardManager.java
+++ client/src/main/java/com/cuhlippa/client/clipboard/ClipboardManager.java
@@ -1,11 +1,13 @@
 package com.cuhlippa.client.clipboard;
 
 import com.cuhlippa.client.storage.LocalDatabase;
+import com.cuhlippa.client.exception.ClipboardHashingException;
 
 import java.awt.*;
 import java.awt.datatransfer.*;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.security.DigestException;
 import java.security.MessageDigest;
 import java.time.LocalDateTime;
 import java.util.Arrays;
@@ -26,5 +28,44 @@ public class ClipboardManager implements ClipboardOwner {
 
     private void regainOwnership(Transferable t) {
         systemClipboard.setContents(t, this);
+    }
+
+    @Override
+    public void lostOwnership(Clipboard c, Transferable t) {
+        try {
+            Thread.sleep(200);
+            processClipboard();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            e.printStackTrace();
+            return;
+        }
+
+        regainOwnership(systemClipboard.getContents(this));
+    }
+
+    private void processClipboard() {
+        Transferable t = systemClipboard.getContents(this);
+        try {
+            if (t != null && t.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                String data = (String) t.getTransferData(DataFlavor.stringFlavor);
+                byte[] contentBytes = data.getBytes();
+                String hash = sha256(contentBytes);
+                ClipboardItem item = new ClipboardItem(ItemType.TEXT, contentBytes, LocalDateTime.now(), hash);
+                db.saveItem(item);
+                System.out.println("Saved new item to clipboard: " + data);
+            } // TODO: add image and file path handling
+        } catch (UnsupportedFlavorException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String sha256(byte[] data) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            return Arrays.toString(digest.digest(data));
+        } catch (Exception e) {
+        throw new ClipboardHashingException("Failed to generate SHA-256 hash for clipboard data", e);
+        }
     }
 }
